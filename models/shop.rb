4d5c5acde544a0c27ed016381fdcad71f994a4e5
Magdalena <m.mandat@gmail.com>
2018-06-20 10:44:31 +0100

    shop

--- models/shop.rb
+++ models/shop.rb
@@ -1,2 +1,28 @@
 require_relative('../db/sql_runner')
-require_relative('./item.rb')
+require_relative('item')
+
+class Shop
+
+attr_accessor :items
+
+  def initialize(items)
+    @items = items
+  end
+
+def single_item_profit
+  profit = @item.sell_price - @item.price
+  return profit
+
+end
+
+  def stock_value
+      total = 0
+      for item in @items
+        total += item.price
+      end
+      return total
+  end
+
+
+
+end
